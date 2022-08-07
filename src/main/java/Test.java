1302c89a676459f3a2735b5a36aa74eb4728c63d
hhf0197 <hhf0197@qq.com>
2022-08-07 19:00:38 +0800

    1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -19,6 +19,7 @@ public class Test {
         //System.out.println(HashMap.class.getName());
         System.out.println("asdnflasfasdf");
         System.out.println("hot-fix");
+        System.out.println("hot-fix 1");
     }
 }
 class HashMapChild extends HashMap{
